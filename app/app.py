60003f0f98ac6a683d0d5bc4fa09179401474761
Stephanie Foskitt <stephanie.foskitt@gmail.com>
2017-01-31 20:40:16 -0800

    update for port and procfile (gunicorn bind)

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 application = Flask(__name__)
 
@@ -6,4 +7,6 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    # application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port=port)
